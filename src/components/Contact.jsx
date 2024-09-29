1db5c9713e175204d7d437ece4ee49b30073e189
unknown <ijazkhayam@gmail.com>
2024-09-29 15:05:01 -0700

    changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ import { GoArrowUpRight } from "react-icons/go";
 const Contact = () => {
   return (
     <>
-      <div className="w-[90%] font-['Poppins'] h-[300px] mx-auto rounded-lg mt-10 bg-[#edd8b7] flex flex-col gap-1">
+      <div className="w-[95%] font-['Poppins'] h-[300px] mx-auto rounded-lg mt-5 bg-[#edd8b7] flex flex-col gap-1">
         <div className="head w-full h-[25%] p-5 flex items-center justify-between">
           <h1 className="w-32">Get in Touch with me.</h1>
           <GoArrowUpRight size={25} />
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Profile = () => {
   return (
     <>
-      <div className="w-[90%] mx-auto h-[500px] bg-white mt-10 rounded-lg ">
+      <div className="w-[95%] mx-auto h-[500px] bg-white mt-5 rounded-lg ">
         <img
           src="/profile.jpg"
           className="w-full h-full object-cover filter grayscale rounded-lg"
